442211ad06ed1c2eb18daa6ec1109e62e363ee3d
Kyle Stubbs <kylestubbs@ucsb.edu>
2021-02-11 18:31:01 -0800

    need to finalize write feature

--- client.py
+++ client.py
@@ -14,7 +14,10 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # might not ne
 
 connectedClientIDs = []
 connectedClientSockets = {}
-replies = []
+replies = 0
+repliesExpected = 0
+sentencesToWrite = []
+sentencesToWriteAfterStart = []
 
 priorityQueue = queue.PriorityQueue()
 # actualQueue = queue.Queue()   won't need if priorityQueue locks in thread
@@ -37,40 +40,27 @@ def do_exit():
     os._exit(0)
 
 
-# connects to all sockets on connect command, puts ID and socket in map and list
-def handle_connect():
-    global connectedClientSockets
-    global connectedClientIDs
-    for i in range(1,4):
-        try:
-            if i != process_id and i not in connectedClientIDs:
-                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                client_socket.connect((socket.gethostname(), int(data[str(i)])))
-                client_socket.sendall(str(process_id).encode())
-                connectedClientIDs.append(i)
-                connectedClientSockets[i] = client_socket
-        except Exception as e:
-            pass
-
-
 # handles send request when write is typed
 def handle_send_request(pid):
     time.sleep(2)
-    message = 'request ' + lamportClock + ' ' + pid
+    message = 'request ' + str(lamportClock) + ' ' + str(pid)
+    print("send: " + message)
     connectedClientSockets[pid].sendall(message.encode())
 
 
 # handles reply command when request is received
 def handle_send_reply(pid):
     time.sleep(2)
-    message = 'reply ' + lamportClock + ' ' + pid
+    message = 'reply ' + str(lamportClock) + ' ' + str(pid)
+    print("send: " + message)
     connectedClientSockets[pid].sendall(message.encode())
 
 
 # handles release command after finished writing
 def handle_send_release(pid):
     time.sleep(2)
-    message = 'release ' + lamportClock + ' ' + pid
+    message = 'release ' + str(lamportClock) + ' ' + str(pid)
+    print("send: " + message)
     connectedClientSockets[pid].sendall(message.encode())
 
 
@@ -78,26 +68,29 @@ def handle_send_release(pid):
 # redirected from handle_bind with the listening socket
 def handle_listen_request(listen_socket, parameter_pid):
     global lamportClock
+    global replies
+    global priorityQueue
     while True:
-        try:
-            messageReceive = listen_socket.recv(1024).decode()
-            messageList = messageReceive.split()
-            messageCode = messageReceive[0]
-            count = int(messageList[1])
-            pid = int(messageList[2])
-            lamportClock = max(count, lamportClock) + 1
-            print(lamportClock)
-            if 'request' == messageCode:
-                priorityQueue.put(tuple((lamportClock,pid)))
-                messageSend = 'reply ' + lamportClock + ' ' + process_id
-                listen_socket.sendall(messageSend.encode())
-                threading.Thread(target=handle_send_reply, args=(parameter_pid)).start()
-            elif 'reply' == messageCode:
-                replies.append([count,pid]) # might only need 1 pid in this function
-            elif 'release' == messageCode:
-                priorityQueue.get() # pops it
-        except IndexError as IE:
-            pass
+        # try:
+        messageReceive = listen_socket.recv(1024).decode()
+        print("messageReceive: " + messageReceive)
+        messageList = messageReceive.split()
+        messageCode = messageList[0]
+        count = int(messageList[1])
+        pid = int(messageList[2])
+        lamportClock = max(count, lamportClock) + 1
+        if 'request' == messageCode:
+            priorityQueue.put(tuple((lamportClock,pid)))
+            # messageSend = 'reply ' + str(lamportClock) + ' ' + process_id
+            # listen_socket.sendall(messageSend.encode())
+            threading.Thread(target=handle_send_reply, args=[parameter_pid]).start()
+        elif 'reply' == messageCode:
+            print("replies += 1")
+            replies += 1 # might only need 1 pid in this function
+        elif 'release' == messageCode:
+            priorityQueue.get() # pops it
+        # except Exception as e:
+        #     pass
 
 
 # constantly listening for when the user enters 'connect' and connections are sent out
@@ -118,13 +111,82 @@ def handle_bind():
 # Approach: create separate queue for client, clear on exit
 # 
 def handle_write_to_server(sentence):
-    print(sentence.split())
+    global replies
+    global repliesExpected
+    global lamportClock
+    releaseCounter = 0
+    stopped = False
+    print("started")
+    while not stopped:
+        # time.sleep(2)
+        # print(repliesExpected)
+        # print(replies)
+        if (repliesExpected == replies):
+            while not priorityQueue.empty():
+                tempTuple = priorityQueue.get()
+                if (tempTuple[0] == process_id):
+                    sentence = sentencesToWrite[0]
+                    sentencesToWrite.pop()
+                    words = sentence.split()
+                    while len(words):
+                        server_socket.sendall(words[0].encode())
+                        words.pop()
+                        ready = server_socket.recv(2048).decode()
+                    releaseCounter += 1
+                else:
+                    priorityQueue.put(tempTuple)
+                    stopped = True
+                    break
+            if (priorityQueue.empty()):
+                stopped = True
+                
 
+    while len(sentencesToWriteAfterStart):
+        sentence = sentencesToWriteAfterStart[0]
+        sentencesToWriteAfterStart.pop()
+        words = sentence.split()
+        while len(words):
+            server_socket.sendall(words[0].encode())
+            words.pop()
+            ready = server_socket.recv(2048).decode()
+
+
+    replies = 0
+    repliesExpected = 0
+    lamportClock += 1
+    for j in range(0, releaseCounter):
+        for i in connectedClientIDs:
+            threading.Thread(target=handle_send_release, args=[i]).start()
+    print("finished")
+
+
+# connects to all sockets on connect command, puts ID and socket in map and list
+def handle_connect():
+    global connectedClientSockets
+    global connectedClientIDs
+    for i in range(1,4):
+        try:
+            if i != process_id and i not in connectedClientIDs:
+                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client_socket.connect((socket.gethostname(), int(data[str(i)])))
+                client_socket.sendall(str(process_id).encode())
+                connectedClientIDs.append(i)
+                connectedClientSockets[i] = client_socket
+        except Exception as e:
+            pass
+
+
+startedWriteThread = False
 
 # handles the users input
 # create multiple threads for requests
 def handle_input():
     global lamportClock
+    global repliesExpected
+    global startedWriteThread
+    global connectedClientIDs
+    global sentencesToWriteAfterStart
+    global sentencesToWrite
     while True:
         try:
             inp = input()
@@ -132,10 +194,18 @@ def handle_input():
                 threading.Thread(target=handle_connect()).start()
             elif 'write' in inp:
                 inp = inp[7:-1]
-                lamportClock += 1   # don't know if I increment here
-                threading.Thread(target=handle_write_to_server, args=([inp])).start()
-                for i in connectedClientIDs:
-                    threading.Thread(target=handle_send_request, args=(i)).start()
+                if not currentlyWriting:
+                    sentencesToWrite.append(inp) # separate list for when it hasn't started
+                    repliesExpected += 2
+                    lamportClock += 1   # don't know if I increment here
+                    priorityQueue.put(tuple((lamportClock, process_id)))
+                    if not startedWriteThread:
+                        threading.Thread(target=handle_write_to_server, args=([inp])).start()
+                        startedWriteThread = True
+                    for i in connectedClientIDs:
+                        threading.Thread(target=handle_send_request, args=[i]).start()
+                else:
+                    sentencesToWriteAfterStart.append(int)
             elif inp == 'exit':
                 do_exit()
         except EOFError:
@@ -155,6 +225,8 @@ if __name__ == "__main__":
 
     threading.Thread(target=handle_input).start()
     threading.Thread(target=handle_bind).start()
+
+    firstReady = server_socket.recv(2048).decode()
 
     # a = tuple((2,2))
     # b = tuple((1,3))
